a82b8c861a6490ba97e26f74fd416b7b1986f9df
Igor Pansini <pansini@outlook.com.br>
2026-05-27 10:55:07 -0300

    created java poc project for mqtt

---
+++ poc-mqtt/src/main/java/com/impansini/poc_mqtt/PocMqttApplication.java
@@ -0,0 +1,13 @@
+package com.impansini.poc_mqtt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocMqttApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocMqttApplication.class, args);
+	}
+
+}
